7a1febc88b02f335eca2a055e5dd1cbf4938ddbd
Katie Burak <katieburak@gmail.com>
2024-10-29 09:59:51 -0700

    Added function documentation

--- R/squared.R
+++ R/squared.R
@@ -1,3 +1,13 @@
+#' Squares a number or vector
+#'
+#' @param x a number to be squared
+#'
+#' @return the square of a number
+#' @export
+#'
+#' @examples
+#' squared(3)
+#' squared(1:3)
 squared <- function(x){
   x^2
 }
